c2a407b7673a48690880f023bf191b15a445c8c8
Yazmin Tinoco <ytip30@gmail.com>
2021-04-05 20:57:20 -0500

    Remove all the entries when you click on the clear button

--- Assets/js/script2.js
+++ Assets/js/script2.js
@@ -10,26 +10,42 @@ if (URLactual.includes("highscores.html")) {
     });
 };
 
- //Get the local storage array 
+//Get the local storage array 
 var entries = localStorage.getItem("allEntries");
 //Lets count how many we have back
 var retrievedObject = JSON.parse(entries);
-console.log(retrievedObject);
 
 //Sort by score
-retrievedObject.sort(function(a, b) {
-    var keyA = new Date(a.sco),
-      keyB = new Date(b.sco);
-    // Compare the 2 dates
-    if (keyA < keyB) return 1;
-    if (keyA > keyB) return -1;
-    return 0;
-  });
-//Show the list of people who played 
-for (let i = 0; i < retrievedObject.length; i++) {
-    //Show all the entries on the page 
-    var li = document.createElement("li");
-    li.textContent = (i+1) +".-"+ retrievedObject[i].init +"--" +retrievedObject[i].sco;
-    list.appendChild(li);
-    
+if (retrievedObject != null) {
+    retrievedObject.sort(function (a, b) {
+        var keyA = new Date(a.sco),
+            keyB = new Date(b.sco);
+        // Compare the 2 dates
+        if (keyA < keyB) return 1;
+        if (keyA > keyB) return -1;
+        return 0;
+    });
+
+    //Show the list of people who played 
+    for (let i = 0; i < retrievedObject.length; i++) {
+        //Show all the entries on the page 
+        var li = document.createElement("li");
+        li.textContent = (i + 1) + ".-" + retrievedObject[i].init + "--" + retrievedObject[i].sco;
+        list.appendChild(li);
+
+    }
 }
+
+
+
+//Remove all the entries when you click on the clear button
+btnClear.addEventListener("click", function () {
+    localStorage.clear();
+    clearAll(list);
+})
+
+function clearAll(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};
